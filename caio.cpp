e92bdda5a7dd227e0b49d0443eb7b53a14724170
Caio Gonçalves <caio.goncalves@cepedi.org.br>
2023-10-18 09:49:28 -0300

    adicionando arquivo

---
+++ caio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int renata[5][5];
+
+    bool exemplo = true;
+    exemplo = false;
+
+    renata[2][0] = 10;
+    renata;
+    cout << renata[2][0];
+}
